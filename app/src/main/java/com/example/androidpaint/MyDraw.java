5ce6916e981f9fcacc74e44f0cf887e13eee499a
valera-salikhov <valera-salikhov@mail.ru>
2021-02-07 22:08:49 +0300

    secondButBordersAreWorst

--- app/src/main/java/com/example/androidpaint/MyDraw.java
+++ app/src/main/java/com/example/androidpaint/MyDraw.java
@@ -13,7 +13,7 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class MyDraw extends View{
-    float x = 500, y = 1000;
+    float x = 500, y = 500;
     float r = 300;
     float d_x = 0, d_y = 0;
     float touch_x = 0, touch_y = 0;
@@ -30,13 +30,15 @@ public class MyDraw extends View{
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
-        if (x == canvas.getWidth() || x == 0) {
-            float sw_d_x = d_x;
-            d_x -= 2 * sw_d_x;
-        }
-        if (y == canvas.getWidth() || y == 0) {
-            float sw_d_y = d_y;
-            d_y -= 2 * sw_d_y;
+        if (x == canvas.getWidth() || x == 0 || y == canvas.getWidth() || y == 0) {
+            if (x == canvas.getWidth() || x == 0) {
+                float sw_d_x = d_x;
+                d_x -= 2 * sw_d_x;
+            }
+            if (y == canvas.getWidth() || y == 0) {
+                float sw_d_y = d_y;
+                d_y -= 2 * sw_d_y;
+            }
         }
         x += d_x;
         y += d_y;
